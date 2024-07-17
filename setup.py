25b8342e5992e3c4d0d48a480cf486485fad8309
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-17 12:20:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.3",
     author="tct123",
     author_email="barney.gale@gmail.com",
-    url="https://github.com/barneygale/myelevate",
+    url="https://github.com/Creative-Media-Group/myelevate",
     license="MIT",
     description="Python library for requesting root privileges",
     long_description=open("README.rst").read(),
